5041e32ddb2b51c4291c0b7b65760031b3594ce9
Joanice Daniel <joanicegaspar@gmail.com>
2024-06-25 16:00:08 +0100

    Add Scaffold Class (AppBar, body, Image...)

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text('I am rich'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Center(
+            child: Text("I'm Riche", style: TextStyle(color: Colors.white))),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Image(
+            image: NetworkImage(
+                'https://images.unsplash.com/photo-1493612276216-ee3925520721?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')),
+      ),
     ),
   ));
 }
