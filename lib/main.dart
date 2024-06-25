84a8c3e0ac4401e1af2ceecf0c9ba8a9f9f2fd9f
Joanice Daniel <joanicegaspar@gmail.com>
2024-06-25 22:22:58 +0100

    Added AssetImages

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,7 @@ void main() {
       ),
       backgroundColor: Colors.blueGrey,
       body: Center(
-        child: Image(
-            image: NetworkImage(
-                'https://images.unsplash.com/photo-1493612276216-ee3925520721?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')),
+        child: Image(image: AssetImage('images/img_rich1.jpg')),
       ),
     ),
   ));
